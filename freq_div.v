80c08e471d612ad5a66a85402999628cc0e2ba64
PreethamKatakam <92195742+PreethamKatakam@users.noreply.github.com>
2024-09-05 08:05:46 +0530

    Update freq_div.v
    design and tb

--- freq_div.v
+++ freq_div.v
@@ -21,3 +21,34 @@ module freq_div (clk,rst,
   end
    
 endmodule
+
+
+
+module freq_div_TB;
+
+  reg clk;
+  reg rst;
+  reg d;
+  wire q1,q2;
+  
+  
+  freq_div freq_div_inst(.clk(clk), .rst(rst),
+          .d(d), .q1(q1), .q2(q2));
+          
+  always #5 clk = ~clk;
+  
+  
+  
+  
+  initial begin
+ 	clk =1'b0;
+	d  =1'b0;
+	end
+	
+
+	initial	begin
+    #100 $stop;
+  end
+  
+
+endmodule
