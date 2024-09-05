b1b115a6777b2554dc869c5fa925ac8f687eb2fe
PreethamKatakam <92195742+PreethamKatakam@users.noreply.github.com>
2024-09-05 08:01:12 +0530

    Add files via upload
    patter code in python
    ((((*))))

---
+++ pat11.py
@@ -0,0 +1,14 @@
+a=5
+for i in range(0,50):
+    for j in range(0, a):
+        print("(",end='')
+    print("*",end='')
+    for j in range(0, a):
+        print(")",end='')
+    a=a+3    
+    
+
+    print()    
+
+        
+    
